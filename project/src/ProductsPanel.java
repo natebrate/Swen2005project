a21f60491fc720daffabf4a75c77254fef8533c2
natebrate <nbrathwaite16@gmail.com>
2020-05-29 13:54:46 -0400

    Create Product Pane

--- project/src/ProductsPanel.java
+++ project/src/ProductsPanel.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.beans.*;
 import javax.swing.*;
 import javax.swing.table.*;
@@ -20,6 +21,18 @@ public class ProductsPanel extends JFrame {
         // TODO add your code here
     }
 
+    private void nameFieldActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void quantityFieldActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void priceFieldActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - unknown
@@ -32,6 +45,15 @@ public class ProductsPanel extends JFrame {
         nameField = new JTextField();
         quantityField = new JTextField();
         priceField = new JTextField();
+        label1 = new JLabel();
+        saveBtn = new JButton();
+        updateBtn = new JButton();
+        deleteBtn = new JButton();
+        clearBtn = new JButton();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        label4 = new JLabel();
+        label5 = new JLabel();
 
         //======== this ========
         var contentPane = getContentPane();
@@ -45,7 +67,7 @@ public class ProductsPanel extends JFrame {
         //---- button1 ----
         button1.setText("Search");
         contentPane.add(button1);
-        button1.setBounds(650, 5, 105, button1.getPreferredSize().height);
+        button1.setBounds(565, 5, 190, button1.getPreferredSize().height);
 
         //======== productPane ========
         {
@@ -54,6 +76,15 @@ public class ProductsPanel extends JFrame {
             //---- productTable ----
             productTable.setModel(new DefaultTableModel(
                 new Object[][] {
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
                     {null, null, null, null},
                     {null, null, null, null},
                     {null, null, null, null},
@@ -96,27 +127,79 @@ public class ProductsPanel extends JFrame {
         //---- allLabel ----
         allLabel.setText("Display All");
         contentPane.add(allLabel);
-        allLabel.setBounds(new Rectangle(new Point(10, 50), allLabel.getPreferredSize()));
+        allLabel.setBounds(70, 435, 165, allLabel.getPreferredSize().height);
 
         //---- IDField ----
-        IDField.setText("ID");
+        IDField.setText("P_ID");
         contentPane.add(IDField);
-        IDField.setBounds(10, 170, 120, IDField.getPreferredSize().height);
+        IDField.setBounds(105, 170, 190, IDField.getPreferredSize().height);
 
         //---- nameField ----
         nameField.setText("Name");
+        nameField.addActionListener(e -> nameFieldActionPerformed(e));
         contentPane.add(nameField);
-        nameField.setBounds(10, 210, 120, nameField.getPreferredSize().height);
+        nameField.setBounds(105, 210, 190, nameField.getPreferredSize().height);
 
         //---- quantityField ----
         quantityField.setText("Quantity");
+        quantityField.addActionListener(e -> quantityFieldActionPerformed(e));
         contentPane.add(quantityField);
-        quantityField.setBounds(10, 255, 120, quantityField.getPreferredSize().height);
+        quantityField.setBounds(105, 255, 190, quantityField.getPreferredSize().height);
 
         //---- priceField ----
         priceField.setText("Price");
+        priceField.addActionListener(e -> priceFieldActionPerformed(e));
         contentPane.add(priceField);
-        priceField.setBounds(10, 305, 120, priceField.getPreferredSize().height);
+        priceField.setBounds(105, 300, 190, priceField.getPreferredSize().height);
+
+        //---- label1 ----
+        label1.setText("Modify Entrees to Table");
+        contentPane.add(label1);
+        label1.setBounds(10, 140, 295, label1.getPreferredSize().height);
+
+        //---- saveBtn ----
+        saveBtn.setText("Save");
+        saveBtn.setForeground(new Color(0, 153, 51));
+        contentPane.add(saveBtn);
+        saveBtn.setBounds(new Rectangle(new Point(70, 355), saveBtn.getPreferredSize()));
+
+        //---- updateBtn ----
+        updateBtn.setText("Update");
+        updateBtn.setForeground(new Color(0, 204, 204));
+        contentPane.add(updateBtn);
+        updateBtn.setBounds(new Rectangle(new Point(155, 355), updateBtn.getPreferredSize()));
+
+        //---- deleteBtn ----
+        deleteBtn.setText("Delete");
+        deleteBtn.setForeground(new Color(204, 0, 0));
+        contentPane.add(deleteBtn);
+        deleteBtn.setBounds(new Rectangle(new Point(70, 395), deleteBtn.getPreferredSize()));
+
+        //---- clearBtn ----
+        clearBtn.setText("Clear");
+        clearBtn.setForeground(Color.yellow);
+        contentPane.add(clearBtn);
+        clearBtn.setBounds(new Rectangle(new Point(155, 395), clearBtn.getPreferredSize()));
+
+        //---- label2 ----
+        label2.setText("Product ID:");
+        contentPane.add(label2);
+        label2.setBounds(new Rectangle(new Point(35, 175), label2.getPreferredSize()));
+
+        //---- label3 ----
+        label3.setText("Product Name:");
+        contentPane.add(label3);
+        label3.setBounds(new Rectangle(new Point(15, 215), label3.getPreferredSize()));
+
+        //---- label4 ----
+        label4.setText("Product Quantity:");
+        contentPane.add(label4);
+        label4.setBounds(new Rectangle(new Point(5, 260), label4.getPreferredSize()));
+
+        //---- label5 ----
+        label5.setText("Product Price:");
+        contentPane.add(label5);
+        label5.setBounds(new Rectangle(new Point(25, 310), label5.getPreferredSize()));
 
         {
             // compute preferred size
@@ -148,5 +231,14 @@ public class ProductsPanel extends JFrame {
     private JTextField nameField;
     private JTextField quantityField;
     private JTextField priceField;
+    private JLabel label1;
+    private JButton saveBtn;
+    private JButton updateBtn;
+    private JButton deleteBtn;
+    private JButton clearBtn;
+    private JLabel label2;
+    private JLabel label3;
+    private JLabel label4;
+    private JLabel label5;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
