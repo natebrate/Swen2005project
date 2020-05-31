4b4b1e6eb551aeadfc194e1551d568a4f9cc6583
CJ <clemjfrancis@gmail.com>
2020-05-31 14:08:10 -0400

    commented out salesDetailsCon

--- project/src/saleDetailsCON.java
+++ project/src/saleDetailsCON.java
@@ -1,36 +1,36 @@
-public class saleDetailsCON {
-
-    public int invoice, P_ID, quantity_sold, sub_total;
-
-
-    saleDetailsCON (int P_ID, int quantity_sold, int sub_total)
-        {
-            this.invoice = invoice;
-            this.P_ID = P_ID;
-            this.quantity_sold = quantity_sold;
-            this.sub_total = sub_total;
-        }
-
-    saleDetailsCON (int invoice, int P_ID, int quantity_sold, int sub_total)
-        {
-            this.invoice = invoice;
-            this.P_ID = P_ID;
-            this.quantity_sold = quantity_sold;
-            this.sub_total = sub_total;
-        }
-
-        public String getInvoice()
-        {
-            return invoice;
-        }
-
-        public int getQuantity_sold()
-        {
-            return quantity_sold;
-        }
-
-        public int getP_ID() { return P_ID; }
-
-        public int getSub_total () { return sub_total; }
-}
-
+//public class saleDetailsCON {
+//
+//    public int invoice, P_ID, quantity_sold, sub_total;
+//
+//
+//    saleDetailsCON (int P_ID, int quantity_sold, int sub_total)
+//        {
+//            this.invoice = invoice;
+//            this.P_ID = P_ID;
+//            this.quantity_sold = quantity_sold;
+//            this.sub_total = sub_total;
+//        }
+//
+//    saleDetailsCON (int invoice, int P_ID, int quantity_sold, int sub_total)
+//        {
+//            this.invoice = invoice;
+//            this.P_ID = P_ID;
+//            this.quantity_sold = quantity_sold;
+//            this.sub_total = sub_total;
+//        }
+//
+//        public String getInvoice()
+//        {
+//            return invoice;
+//        }
+//
+//        public int getQuantity_sold()
+//        {
+//            return quantity_sold;
+//        }
+//
+//        public int getP_ID() { return P_ID; }
+//
+//        public int getSub_total () { return sub_total; }
+//}
+//
